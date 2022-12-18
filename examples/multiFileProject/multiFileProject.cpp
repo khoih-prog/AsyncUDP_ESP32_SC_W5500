d1194e4684bf6a9d355cfe459c9be0f2791453b4
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-18 15:18:32 -0500

    v2.0.0 for `ESP32_S3 + LwIP W5500`
    ### Initial Releases v2.0.0
    
    1. Initial coding to port [AsyncUDP](https://github.com/espressif/arduino-esp32/tree/master/libraries/AsyncUDP) to ESP32_S3 boards using `LwIP W5500 Ethernet`
    2. Bump up to v2.0.0 to sync with [AsyncUDP v2.0.0](https://github.com/espressif/arduino-esp32/tree/master/libraries/AsyncUDP)

---
+++ examples/multiFileProject/multiFileProject.cpp
@@ -0,0 +1,12 @@
+/****************************************************************************************************************************
+  multiFileProject.cpp
+  AsyncUDP_ESP32_SC_W5500 is a Async UDP library for the ESP32_SC_W5500 (ESP32S2/S3/C3 + LwIP W5500)
+
+  Based on and modified from ESPAsyncUDP Library (https://github.com/me-no-dev/ESPAsyncUDP)
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncUDP_ESP32_SC_W5500
+  Licensed under GPLv3 license
+*****************************************************************************************************************************/
+
+// To demo how to include files in multi-file Projects
+
+#include "multiFileProject.h"
---
+++ examples/multiFileProject/multiFileProject.h
@@ -0,0 +1,43 @@
+/****************************************************************************************************************************
+  multiFileProject.h
+  AsyncUDP_ESP32_SC_W5500 is a Async UDP library for the ESP32_SC_W5500 (ESP32S2/S3/C3 + LwIP W5500)
+
+  Based on and modified from ESPAsyncUDP Library (https://github.com/me-no-dev/ESPAsyncUDP)
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncUDP_ESP32_SC_W5500
+  Licensed under GPLv3 license
+*****************************************************************************************************************************/
+
+// To demo how to include files in multi-file Projects
+
+#pragma once
+
+#if !( defined(ESP32) )
+  #error This code is designed for (ESP32S2/S3/C3 + LwIP W5500) to run on ESP32 platform! Please check your Tools->Board setting.
+#endif
+
+#include <Arduino.h>
+
+#define ASYNC_UDP_ESP32_SC_W5500_DEBUG_PORT      Serial
+
+// Use from 0 to 4. Higher number, more debugging messages and memory usage.
+#define _ASYNC_UDP_ESP32_SC_W5500_LOGLEVEL_      2
+
+//////////////////////////////////////////////////////////
+
+// Optional values to override default settings
+// Don't change unless you know what you're doing
+//#define ETH_SPI_HOST        SPI3_HOST
+//#define SPI_CLOCK_MHZ       25
+
+// Must connect INT to GPIOxx or not working
+//#define INT_GPIO            4
+
+//#define MISO_GPIO           13
+//#define MOSI_GPIO           11
+//#define SCK_GPIO            12
+//#define CS_GPIO             10
+
+//////////////////////////////////////////////////////////
+
+// Can be included as many times as necessary, without `Multiple Definitions` Linker Error
+#include "AsyncUDP_ESP32_SC_W5500.hpp"
